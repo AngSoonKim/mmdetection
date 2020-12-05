83b72499acc0b85647e0cb4d19d53ade7b008e8b
AngSoonKim <65056781+AngSoonKim@users.noreply.github.com>
2020-12-05 14:12:10 +0800

    Update my_dataset.py

--- mmdet/datasets/my_dataset.py
+++ mmdet/datasets/my_dataset.py
@@ -1,1 +1,10 @@
-my_dataset.py
+from .coco import CocoDataset
+from .registry import DATASETS
+from .xml_style import XMLDataset
+
+### IMPORT YOUR DATASET FORMAT AND PASS IT IN MYDATASET FUNC
+
+@DATASETS.register_module
+class MyDataset(XMLDataset):
+	
+	CLASSES=('gauze') ##ADD ALL YOUR CLASSES
